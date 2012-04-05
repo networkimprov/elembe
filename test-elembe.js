588fb4500d9a519a89803032db6b2a0d45732267
networkimprov <github@networkimprov.net>
2012-04-04 19:54:30 -0700

    add: test-elembe.js "make" supports multiple nodes

--- test-elembe.js
+++ test-elembe.js
@@ -1,7 +1,7 @@
 
 // test directory manager for elembe
 //
-// make node-name test-name
+// make test-name node-name [node-name2 ...]
 //   add a new test
 //
 // update
@@ -15,7 +15,7 @@ var sNextdata = 'Nextdata';
 var sRecording = 'recording';
 var sData = 'data-test';
 var sDataArg = sData.slice(5);
-var sPort = 8999;
+var sPort = 8900;
 
 process.on('SIGINT', function() {});
 
@@ -25,37 +25,47 @@ function main(argv) {
 
   switch(argv[1]) {
   case 'make':
-    var aNode = sDir+argv[2]+'/';
-    var aNext = aNode+sNextdata;
-    var aItem = readdir(aNode);
-    if (!aItem || !aItem.length) {
-      aItem = '00';
-    } else {
-      aItem = aItem.sort().pop();
-      if (aItem.charAt(2) !== '_')   return console.log('unexpected last test "'+aItem+'" in '+aNode);
-      if (aItem.slice(0,2) === 'zz') return console.log(aNode+' has max number of tests');
-      aItem = (parseInt(aItem.slice(0,2), 36) +1).toString(36);
+    if (argv.length < 4) return console.log('missing arguments');
+    var aCmd = [];
+    for (var aN=0; aN+3 < argv.length; ++aN) {
+      var aData = sData+aN;
+      var aRecording = sRecording+aN;
+      if (stat(aData))      return console.log(aData      +' already exists');
+      if (stat(aRecording)) return console.log(aRecording +' already exists');
+      var aDataArg = sDataArg+aN;
+      var aPort = sPort+aN;
+      var aNode = sDir+argv[aN+3]+'/';
+      var aNext = aNode+sNextdata;
+      var aItem = readdir(aNode);
+      if (!aItem || !aItem.length) {
+        aItem = 'a0';
+      } else {
+        aItem = aItem.sort().pop();
+        if (aItem.charAt(2) !== '_')   return console.log('unexpected last test "'+aItem+'" in '+aNode);
+        if (aItem.slice(0,2) === 'zz') return console.log(aNode+' has max number of tests');
+        aItem = (parseInt(aItem.slice(0,2), 36) +1).toString(36);
+      }
+      var aTest = aNode+aItem+'_'+argv[2];
+      if (stat(aTest)) return console.log(aTest+' already exists');
+      mkdir(aNode);
+      var aHasNext = stat(aNext);
+      aCmd.push(
+        (aHasNext ? 'cp -r '+aNext+' '+aData+';' : '')+'\
+        echo starting node '+argv[aN+3]+' on port '+aPort+'; \
+        node elembe.js -data '+aDataArg+' -port '+aPort+' -rec '+aRecording+'; \
+        mkdir '+aTest+'; \
+        mv '+aRecording+' '+aTest+'/'+sRecording+';\n'+
+        (aHasNext ? 'mv '+aNext+' '+aTest+'/'+sData+';' : '')+' \
+        mv '+aData+' '+aNext+'; ' );
     }
-    var aTest = aNode+aItem+'_'+argv[3];
-    if (stat(aTest))      return console.log(aTest      +' already exists');
-    if (stat(sData))      return console.log(sData      +' already exists');
-    if (stat(sRecording)) return console.log(sRecording +' already exists');
-    mkdir(aNode);
-    var aHasNext = stat(aNext);
-    var aCmd = 
-      (aHasNext ? 'cp -r '+aNext+' '+sData+';' : '')+'\
-      echo starting elembe on port '+sPort+'; \
-      node elembe.js -data '+sDataArg+' -port '+sPort+' -rec '+sRecording+'; \
-      mkdir '+aTest+'; \
-      mv '+sRecording+' '+aTest+';\n'+
-      (aHasNext ? 'mv '+aNext+' '+aTest+'/'+sData+';' : '')+' \
-      mv '+sData+' '+aNext+'; ';
-    run(aCmd, function(exitcode) {
-      if (exitcode)
-        run('rm -r '+sData+' '+sRecording, function() { console.log('test aborted') });
+    for (var aN=0; aN < aCmd.length; ++aN)
+      run(aCmd[aN], aN.toString(), fDone);
+    function fDone(n) {
+      if (n)
+        run('rm -rf '+sData+n+' '+sRecording+n, function() { console.log('test aborted') });
       else
         console.log('test created');
-    });
+    }
     break;
 
   case 'update':
@@ -84,9 +94,9 @@ function main(argv) {
           rm -r '+aNodeDir+aData+'; \
           mv '+sData+' '+aNodeDir+aData+'; \
           echo updated '+aNode[nodeN]+' '+aTest[testN]+';';
-        run(aCmd, function(exitcode) {
-          if (exitcode)
-            run('rm -r '+sData, function() { console.log('update failed') });
+        run(aCmd, 1, function(n) {
+          if (n)
+            run('rm -rf '+sData, function() { console.log('update failed') });
           else
             fTest(++testN);
         });
@@ -105,7 +115,7 @@ function main(argv) {
     aDir.sort();
     var aStartData;
     var aTests = '';
-    for (var a=0; a < aDir.length; ++a) {
+    for (var a=1; a < aDir.length; ++a) {
       if (aDir[a] < aFirst)
         continue;
       if (!aStartData)
@@ -118,9 +128,9 @@ function main(argv) {
     var aCmd =
       (stat(aStartData) ? 'cp -r '+aStartData+' . ;' : '')+' \
       node elembe.js -data '+sDataArg+' -port '+sPort+' -play '+aTests+';';
-    run(aCmd, function(exitcode) {
+    run(aCmd, function() {
       if (stat(sData))
-        run('rm -r '+sData, function(){});
+        run('rm -rf '+sData, function(){});
     });
     break;
 
@@ -132,7 +142,11 @@ function main(argv) {
 var nChild = require('child_process');
 var nFs = require('fs');
 
-function run(iCommand, iCallback) {
+function run(iCommand, iErrValue, iCallback) {
+  if (arguments.length < 3) {
+    iCallback = iErrValue;
+    iErrValue = undefined;
+  }
   var aLine = iCommand.split(/\s*;\s*/);
   fRun(0);
   function fRun(aN) {
@@ -144,13 +158,13 @@ function run(iCommand, iCallback) {
         break;
     }
     if (aN === aLine.length)
-      return process.nextTick(function() { iCallback(0) });
+      return process.nextTick(iCallback);
     if (aArg[aArg.length-1] === '')
       aArg.pop();
     var aCmd = aArg.shift();
     var aC = nChild.spawn(aCmd, aArg, {customFds:[0,1,2], env:process.env});
     aC.on('exit', function(code) {
-      if (code) return iCallback(code);
+      if (code) return iCallback(iErrValue);
       fRun(++aN);
     });
   }
